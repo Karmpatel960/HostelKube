5b5ec181ba65424dd5a5c092b848d72bd7bc6f15
karmpatel960 <96860884+Karmpatel960@users.noreply.github.com>
2023-08-13 19:42:46 +0530

    images added

---
+++ hostelkube_frontend/lib/src/theme/theme.dart
@@ -0,0 +1,11 @@
+    // --primary-100:#FF4081;
+    // --primary-200:#ff79b0;
+    // --primary-300:#ffe4ff;
+    // --accent-100:#536DFE;
+    // --accent-200:#ffffff;
+    // --text-100:#333333;
+    // --text-200:#5c5c5c;
+    // --bg-100:#F5F5F5;
+    // --bg-200:#ebebeb;
+    // --bg-300:#c2c2c2;
+      
